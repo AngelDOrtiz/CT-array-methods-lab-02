8e11b6115527ff65dab39eff2680938a94842add
AngelDOrtiz <aortizghs@gmail.com>
2021-06-08 15:51:50 -0700

    lab and first steps initialized

---
+++ __tests__/map.test.js
@@ -0,0 +1,5 @@
+describe('map(arr, callback)', () => {
+    it('Takes an array and callback of signature item => {} and creates a new array with the return value of each called callback', () => {
+        
+    });
+});
