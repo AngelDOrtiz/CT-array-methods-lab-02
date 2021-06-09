9c5a92b79cbfa21279cb091827871963f49113ca
AngelDOrtiz <aortizghs@gmail.com>
2021-06-09 10:09:59 -0700

    filter function working

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -1,9 +1,18 @@
-import { mappingFunction } from '../map.js';
+import { mappingFunction, filterFunction } from '../map.js';
 
 describe('map(arr, callback)', () => {
   it('Takes an array and callback of signature item => {} and creates a new array with the return value of each called callback', () => {
     const expectedArray = [1, 2, 3, 4];
     const mappedArray = mappingFunction(expectedArray, (item) => {return item + 1;});
     expect(mappedArray).toEqual([2, 3, 4, 5]);
+  });
+});
+
+describe('filter(arr, callback', () => {
+  it('Takes an Array and callback of signature item => {} and creates a new Array with all items whose callback returned true or a truthy value.', () => {
+    const testingArray = ['cool', 'cat', 'bumper', 'six-shooter'];
+    const filteredArray = filterFunction(testingArray, (item) => {return item.length < 5;});
+    expect(filteredArray).toEqual(['cool', 'cat']);
+
   });
 });
--- map.js
+++ map.js
@@ -6,3 +6,15 @@ export function mappingFunction(arr, callback) {
   }
   return booger;
 }
+
+export function filterFunction(arr, callback) {
+  const array = [];
+  let j = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      array[j] = arr[i];
+      j++;
+    }
+  }
+  return array;
+}
